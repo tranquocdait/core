facaa415871b8d0380c9804e196593a410e0ac61
unknown <tranquocdaiit@gmail.com>
2022-08-20 21:44:14 +0700

    change swagger version

--- src/main/java/com/example/core/controller/HelloController.java
+++ src/main/java/com/example/core/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/helloWord", method = RequestMethod.POST)
+    @RequestMapping(value = "/helloWord", method = RequestMethod.GET)
     public String custom() {
         return "Hello word! This is the first code for the core project";
     }
